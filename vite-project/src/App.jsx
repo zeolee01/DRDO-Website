411007f0db61c273968027ed6206efd872b4ecca
zeolee01 <114869179+zeolee01@users.noreply.github.com>
2024-07-10 01:12:21 +0530

    commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css"
+import Home from "./Pages/Home/Home"
 
 function App() {
-  return <></>
+  return (
+    <div className="bg-teal-950 h-full w-full pb-5">
+      <Home />
+    </div>
+  )
 }
 
 export default App
---
+++ vite-project/src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Croucel from "../../components/Croucel"
+import Noitce from "../../components/Noitce"
+
+const Home = () => {
+  return (
+    <div>
+      <Croucel />
+      <Noitce />
+    </div>
+  )
+}
+
+export default Home
