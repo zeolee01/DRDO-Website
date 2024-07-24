943fc6c7a2acaa7aaf647a592b9df62c9cb574af
zeolee01 <114869179+zeolee01@users.noreply.github.com>
2024-07-25 00:19:26 +0530

    employ notice updated

--- vite-project/src/components/Employee/Noitce.jsx
+++ vite-project/src/components/Employee/Noitce.jsx
@@ -1,76 +1,70 @@
 import React, { useState, useEffect, useRef } from "react"
-import { FaEdit } from "react-icons/fa"
 import "../../index.css"
+import axios from "axios"
 
 const Notice = () => {
   const [notices, setNotices] = useState([])
-  const [newNotice, setNewNotice] = useState({ content: "", type: "NEW" }) // Changed to use 'type' instead of 'date'
-  const [editNoticeId, setEditNoticeId] = useState(null)
-  const [editNotice, setEditNotice] = useState({ content: "", type: "" }) // Changed to use 'type' instead of 'date'
-
-  const editRef = useRef()
+  const [error, setError] = useState(null)
+  const [newNotice, setNewNotice] = useState({ content: "", type: "" })
 
   useEffect(() => {
-    // Fetch notices from local storage when the component mounts
-    const storedNotices = JSON.parse(localStorage.getItem("notices")) || []
-    setNotices(storedNotices)
-  }, [])
-
-  const saveNoticesToLocalStorage = (notices) => {
-    localStorage.setItem("notices", JSON.stringify(notices))
-  }
+    const fetchNotice = async () => {
+      try {
+        const response = await axios.get("http://localhost:3002/getnotice")
+        setNotices(response.data)
+      } catch (error) {
+        console.error(`error fetching notices:`, error)
+        setError(error.message)
+      }
+    }
+    fetchNotice()
+  }, [notices])
 
   const handleInputChange = (e) => {
     const { name, value } = e.target
     setNewNotice({ ...newNotice, [name]: value })
   }
 
-  const handleEditInputChange = (e) => {
-    const { name, value } = e.target
-    setEditNotice({ ...editNotice, [name]: value })
-  }
-
-  const addNotice = (e) => {
+  const addNotice = async (e) => {
     e.preventDefault()
-    const newId = notices.length > 0 ? notices[notices.length - 1].id + 1 : 1
-    const noticeToAdd = { id: newId, ...newNotice }
-    const updatedNotices = [...notices, noticeToAdd]
-    setNotices(updatedNotices)
-    saveNoticesToLocalStorage(updatedNotices)
-    setNewNotice({ content: "", type: "NEW" }) // Reset type to "NEW"
-  }
-
-  const removeNotice = (id) => {
-    const updatedNotices = notices.filter((notice) => notice.id !== id)
-    setNotices(updatedNotices)
-    saveNoticesToLocalStorage(updatedNotices)
-  }
-
-  const saveEdit = (id) => {
-    const updatedNotices = notices.map((notice) =>
-      notice.id === id
-        ? { ...notice, content: editNotice.content, type: editNotice.type }
-        : notice
-    )
-    setNotices(updatedNotices)
-    saveNoticesToLocalStorage(updatedNotices)
-    setEditNoticeId(null)
-    setEditNotice({ content: "", type: "" }) // Reset type
-  }
-
-  const handleClickOutside = (e) => {
-    if (editRef.current && !editRef.current.contains(e.target)) {
-      setEditNoticeId(null)
-      setEditNotice({ content: "", type: "" }) // Reset type
+    const { content: newNoti, type: newTy } = newNotice
+    setError(null)
+    try {
+      const response = await axios.post("http://localhost:3002/addnotice", {
+        content: newNoti,
+        type: newTy.toUpperCase(),
+      })
+      if (!response.ok) {
+        throw new Error(`Failed to create notice: ${response.statusText}`)
+      }
+      setNewNotice({ content: "", type: "" })
+      setNotices([...notices, response.data])
+      await fetchNotice()
+    } catch (error) {
+      console.error("There was an error creating the notice:", error)
+      if (error.response && error.response.data) {
+        setError(error.response.data.message)
+      } else {
+        setError("There was a network error or unexpected issue.")
+      }
     }
   }
 
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside)
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside)
+  const removeNotice = async (id) => {
+    try {
+      const response = await axios.delete(
+        `http://localhost:3002/deletenotice/${id}`
+      )
+
+      if (!response.ok) {
+        throw new Error(`Failed to delete notice: ${response.statusText}`)
+      }
+      console.log(response.data.message + " (Notice deleted!)")
+      setNotices(notices.filter((notice) => notice.id !== id))
+    } catch (error) {
+      console.error("There was an error deleting the notice:", error)
     }
-  }, [])
+  }
 
   return (
     <div className="py-20 w-full notice-background font-titillium">
@@ -78,100 +72,33 @@ const Notice = () => {
         Notifications
       </h1>
       <div className="w-full flex flex-col items-center">
+        {/* check----- */}
         <div className="notice-list-container w-7/12 max-h-80 overflow-y-auto">
           {notices.map((notice) => (
             <div
               key={notice.id}
               className="notice-card bg-white p-6 mb-4"
               tabIndex="0"
-              ref={editNoticeId === notice.id ? editRef : null}
             >
-              {editNoticeId === notice.id ? (
-                <div className="flex flex-col w-full">
-                  <input
-                    type="text"
-                    name="content"
-                    value={editNotice.content}
-                    onChange={handleEditInputChange}
-                    className="px-4 py-2 border rounded-lg w-full mb-2"
-                    placeholder="Enter new content"
-                  />
-                  <input
-                    type="text"
-                    name="type"
-                    value={editNotice.type}
-                    onChange={handleEditInputChange}
-                    className="px-4 py-2 border rounded-lg w-full mb-2"
-                    placeholder="NEW/UPDATE"
-                  />
-                  <div className="flex">
-                    <button
-                      onClick={() => saveEdit(notice.id)}
-                      className="px-4 py-2 bg-green-500 text-black text-bold rounded-lg hover:bg-green-700 mr-2"
-                    >
-                      Save
-                    </button>
+              <div className="flex justify-between items-center w-full">
+                <div className="content">{notice.text}</div>
+                <div className="flex items-center">
+                  <div
+                    className={`type ${
+                      notice.type === "NEW" ? "text-white" : "text-white"
+                    } bg-red-600 rounded-full px-2 py-1`}
+                  >
+                    {notice.type}
                   </div>
+                  {/* <div>
+                    const timeElapsed = Date.now() const today = new
+                    Date(timeElapsed)
+                  </div> */}
                 </div>
-              ) : (
-                <div className="flex justify-between items-center w-full">
-                  <div className="content">{notice.content}</div>
-                  <div className="flex items-center">
-                    <div
-                      className={`type ${
-                        notice.type === "NEW" ? "text-white" : "text-white"
-                      } bg-red-600 rounded-full px-2 py-1`}
-                    >
-                      {notice.type}
-                    </div>
-                    <FaEdit
-                      className="ml-2 cursor-pointer text-gray-500 hover:text-blue-700"
-                      onClick={() => {
-                        setEditNoticeId(notice.id)
-                        setEditNotice({
-                          content: notice.content,
-                          type: notice.type,
-                        })
-                      }}
-                    />
-                    <button
-                      onClick={() => removeNotice(notice.id)}
-                      className="ml-2 px-2 py-1 bg-gray-800 text-white rounded-lg hover:bg-gray-600"
-                    >
-                      Delete
-                    </button>
-                  </div>
-                </div>
-              )}
+              </div>
             </div>
           ))}
         </div>
-        <form onSubmit={addNotice} className="mt-6 text-center w-7/12">
-          <input
-            type="text"
-            name="content"
-            value={newNotice.content}
-            onChange={handleInputChange}
-            placeholder="Enter notification"
-            className="px-4 py-2 border rounded-lg w-full mb-2"
-            required
-          />
-          <input
-            type="text"
-            name="type"
-            value={newNotice.type}
-            onChange={handleInputChange}
-            placeholder="NEW/UPDATE"
-            className="px-4 py-2 border rounded-lg w-full mb-2"
-            required
-          />
-          <button
-            type="submit"
-            className="px-6 py-2 bg-gray-800 text-white rounded-lg hover:bg-gray-600"
-          >
-            Add Notification
-          </button>
-        </form>
       </div>
     </div>
   )
