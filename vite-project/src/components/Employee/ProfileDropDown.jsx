94b7da783b396c6cb16e436999e3368ca40563ee
akshaya-cp <akshayasingh61@gmail.com>
2024-07-26 01:20:35 +0530

    added yes/no functionality in Log Out

--- vite-project/src/components/Employee/ProfileDropDown.jsx
+++ vite-project/src/components/Employee/ProfileDropDown.jsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from "react-router-dom";
 import '../ProfileDropDown.css';
 
 const ProfileDropDown = () => {
+  const [logoutDropdown, setLogoutDropdown] = useState(false);
+
+  const toggleLogoutDropdown = () => {
+    setLogoutDropdown(!logoutDropdown);
+  };
+
   return (
     <div className='flex flex-col ProfileDropDown'>
-      <ul className='flex flex-col gap-4 font-titillium '>
-        <Link className="text-black hover:translate-x-2 transition-transform duration-200">Profile</Link>
-        <Link className="text-black hover:translate-x-2 transition-transform duration-200">Edit</Link>
-        <Link to="/employeelogin" className="text-black hover:translate-x-2 transition-transform duration-200">Logout</Link>
+      <ul className='flex flex-col gap-4 font-titillium'>
+        <Link className="text-black hover:translate-x-2 transition-transform duration-200">
+          Profile
+        </Link>
+        <Link className="text-black hover:translate-x-2 transition-transform duration-200">
+          Edit
+        </Link>
+        <div className="relative">
+          <Link
+            to="#"
+            className="text-black hover:translate-x-2 transition-transform duration-200"
+            onClick={toggleLogoutDropdown}
+          >
+            Logout
+          </Link>
+          {logoutDropdown && (
+            <div
+              className="absolute top-0 left-[-50%] mt-1 z-50 flex flex-col bg-white border border-gray-200 rounded shadow-lg"
+            >
+              <Link to="/employeelogin" className="text-black px-4 py-2 hover:bg-gray-200">
+                Yes
+              </Link>
+              <button
+                className="text-black px-4 py-2 hover:bg-gray-200"
+                onClick={toggleLogoutDropdown}
+              >
+                No
+              </button>
+            </div>
+          )}
+        </div>
       </ul>
     </div>
   );
